1437a4619eb1fc8db3f1055cc032cae46e6fc609
JSU <nonexistent@jsu.edu>
2019-11-11 14:38:17 -0600

    post class commit

--- src/java/edu/jsu/mcis/cs425/project2/BeanApplicant.java
+++ src/java/edu/jsu/mcis/cs425/project2/BeanApplicant.java
@@ -3,7 +3,36 @@ package edu.jsu.mcis.cs425.project2;
 public class BeanApplicant {
     
     private String username;
+    private String displayName;
+    private int id;
+    
+    
+    public void setUserInfo(){
+        
+        Database db = new Database();
+        HashMap<String, String> userinfo = db.getUserInfo(username);
+        id = Integer.parseInt(userinfo.get("id"));
+        displayName = userinfo.get("displayname");
 
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
     public String getUsername() {
         return username;
     }
---
+++ src/java/edu/jsu/mcis/cs425/project2/Database.java
@@ -0,0 +1,76 @@
+
+package edu.jsu.mcis.cs425.project2;
+
+
+
+import java.sql.ResultSetMetaData;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.json.simple.JSONArray;
+
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+//attempting to fix parsing error
+public class Database {
+    //
+    String sessID;
+    
+    
+    public HashMap getUserInfo(String username) throws SQLException {
+        
+        HashMap<String, String> results = null;
+       
+        
+        try { 
+        
+            Connection conn = getConnection();
+            
+            String query = "SELECT * FROM  'user' WHERE username = ?";
+            PreparedStatement pstatement = conn.prepareStatement(query);
+            pstatement.setString(1, username);
+            
+            boolean hasresults = pstatement.execute();
+            
+            if (hasresults){
+               
+                ResultSet resultset = pstatement.getResultSet();
+                
+                if (resultset.next()){
+                    
+                    
+                }
+                
+                
+            }
+            
+            return results;
+        }
+    }
+    
+    private Connection getConnection() {
+        
+        Connection conn = null;
+        
+        try {
+            
+            Context envContext = new InitialContext();
+            Context initContext  = (Context)envContext.lookup("java:/comp/env");
+            DataSource ds = (DataSource)initContext.lookup("jdbc/db_pool");
+            conn = ds.getConnection();
+            
+        }        
+        catch (Exception stinkystinky) { stinkystinky.printStackTrace(); }
+        
+        return conn;
+
+    }
+}
